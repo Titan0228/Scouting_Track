8ee6c8eb8e10656616397cf9951c57b0ccd133b4
Nicholas <nicholas.h.chiang@gmail.com>
2018-05-10 13:55:20 -0700

    Added BSA item types
    Added BSA item types (i.e. Scout Ranks, Merit Badges) and adapted original ToDoList class to better fit scout tracking.

--- ToDoList.py
+++ ToDoList.py
@@ -1,10 +1,21 @@
 import time
 import datetime
 
+#creates a list of all 137 BSA Merit Badges as of 2018 from a text document called Merit_Badge_List.txt:
 
-class ToDoList(object):
+merit_badges = None
 
-    to_do_list = {
+with open("Merit_Badge_List.txt", "r") as MeritBadgeList:
+    merit_badges = str(MeritBadgeList.read())
+
+merit_badges = merit_badges.split("\n")
+
+
+#creates class for achievement lists (lists of completed ranks)
+
+class CompletedList(object):
+
+    completed_dict = {
         }
     current_date = ((datetime.date.today().strftime("%B")) + " " + (datetime.date.today().strftime("%d")))
 
@@ -14,29 +25,40 @@ class ToDoList(object):
         self.date = self.current_date
 
     def add_item(self):
-        
-        #adds the inputed item into to_do_list under the correlating datetime
-        self.to_do_list[str(datetime.datetime.now())] = str(input("Item: "))
+                                 
+        #adds the inputed item into completed_dict under the correlating time stamp
+        item = str(input("Item: "))
+        item_list = [item]
+        self.completed_dict[str(datetime.datetime.now())] = item_list
+
+        #adds achievement type of the inputed item (i.e. Merit Badge, Scout Rank, or No Type)
+        if (item == "Tenderfoot" or item == "Second Class" or item == "First Class" or item == "Star Scout" or item == "Life Scout" or item == "Eagle Scout"):
+            item_list.append("Scout Rank")
+            
+        elif (item in merit_badges):
+            item_list.append("Merit Badge")
+
+        else:
+            item_list.append("No Type")
 
     def print_list(self):
 
-        print ("Date:  Item:")
+        print ("Date:  Achievement:  Type:")
 
-        for item in self.to_do_list:
+        for item_time in self.completed_dict:
             
-            #Puts datetime into month/day of month format
-            time = str(item)
+            #Puts time stamp into month/day of month format
+            time = str(item_time)
             time2 = ((time.split("-")))
             time3 = (time2[2].split(" "))
 
-            #prints out the date in month/day format and the item with spaces in between
-            print ((time3[0] + "/" + time2[1]) + "  " + self.to_do_list[item])
+            #prints out the date in month/day format and the item_list's first value (which is the original item) with spaces in between
+            print ((time3[0] + "/" + time2[1]) + "  " + self.completed_dict[item_time][0] + "  " + self.completed_dict[item_time][1])
 
 
-my_list = ToDoList("My To Do List")
+
+
+my_list = CompletedList("My To Do List")
 
 my_list.add_item()
-my_list.add_item()
-
 my_list.print_list()
-
